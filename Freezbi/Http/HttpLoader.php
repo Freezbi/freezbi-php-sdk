87c5ac71c0fe846bf26a892cb5e007ce2cc6ff16
JRK <jrkdev@gmail.com>
2015-10-21 13:42:13 +0200

    timeout

--- Freezbi/Http/HttpLoader.php
+++ Freezbi/Http/HttpLoader.php
@@ -35,7 +35,7 @@ class HttpLoader
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 400);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_MAXREDIRS, 2); //only 2 redirects
